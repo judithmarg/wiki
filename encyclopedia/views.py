dbdb29e05adbc15af2566f6f790a5ceb3d7e7e7e
judithMarg <judithmargaritapacollalli@gmail.com>
2023-08-06 23:33:47 -0400

    add search with errors

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,6 +7,7 @@ import markdown2
 
 class NewSearchForm(forms.Form):
     busqueda = forms.CharField(label='Search Encyclopedia')
+    priority = forms.IntegerField(label='Priority', min_value=1, max_value=5 )
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -27,20 +28,24 @@ def search(request):
     if request.method == 'POST':
         form = NewSearchForm(request.POST)
         if form.is_valid():
+            print('es valido')
             cadena = form.cleaned_data['busqueda']
             entradas = util.list_entries()
             for entrada in entradas:
                 if cadena in entrada:
                     posibilidades.append(entrada)
-            return render(request, 'encyclopedia/pageResults.html', {
-                'posibilidades' : posibilidades 
-            })
-        else:
-            return render(request, 'encyclopedia/pageResults.html', {
-                'posibilidades': posibilidades
-            })
-    return render(request, 'encyclopedia/pageResults.html', {
-        'posibilidades': posibilidades
+                    print('entro una posibilidad')
+                return render(request, 'encyclopedia/pageResults.html', {
+                    'posibilidades' : posibilidades ,
+                    'form' : NewSearchForm
+                })
+        ##else:
+            ##print('no entro una posibilidad')
+            ##return render(request, 'encyclopedia/pageResults.html', {
+            ##    'posibilidades': posibilidades
+            ##})
+    return render(request, 'encyclopedia/layout.html', {
+        'form': NewSearchForm()
     })
 
 
